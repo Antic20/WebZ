fc2373150a187ab8f04df64d0329fbeaa4315b35
Antic20 <aleksandarantic.inf@gmail.com>
2023-09-30 14:56:37 +0200

    Commit
    Date : 30.09.2023

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebZ.Models;
+
+namespace WebZ.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
